cf612eb72d4e518ef21a861390f2691840f35ab6
bsc-com-35-19 <bsc-com-35@unima.ac.mw>
2024-03-27 09:35:13 -0700

    have created a landing page with a nav bar and  nav links to repective pages

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+ 
+const Home = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'centre',
+        alignItems: 'centre',
+        height: '100vh'
+      }}
+    >
+      <h1>welcome to agricultural commodity price projection</h1>
+    </div>
+  );
+};
+ 
+export default Home;
