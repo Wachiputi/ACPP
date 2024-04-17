34f7d4bb087ca76e0602c98b5d21342f464f26dc
bsc-com-35-19 <bsc-com-35@unima.ac.mw>
2024-04-17 17:47:17 +0200

    form added to container

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -6,7 +6,16 @@ const Signin = () => {
     <div>
       <p>
 
-      "Welcome to our Agricultural Commodity Price Projection! Step into the future of farming with us as we unveil our latest forecasts for the upcoming harvest season. Discover the anticipated prices of staple crops like wheat, corn, and soybeans, and gain valuable insights into market trends and fluctuations. Whether you're a farmer, trader, or enthusiast, our projections will guide you towards informed decisions and successful ventures in the agricultural industry. Join us on this exciting journey of growth and prosperity in agriculture!"
+      "Welcome to our Agricultural Commodity Price Projection!
+      Step into the future of farming with us as we unveil our 
+      latest forecasts for the upcoming harvest season. Discover
+      the anticipated prices of staple crops like wheat, corn,
+      and soybeans, and gain valuable insights into market trends
+      and fluctuations. Whether you're a farmer, trader, or enthusiast,
+      our projections will guide you towards informed decisions 
+      and successful ventures in the agricultural industry. 
+      Join us on this exciting journey of growth and prosperity in
+       agriculture!"
 
       </p>
 
@@ -24,7 +33,7 @@ const Signin = () => {
     <div className="text-center"> 
       <button type="button" className="btn btn-primary px-4">Sign In</button>
     </div>
-    <h3 className="createAcc"> dont have an account ?</h3>
+    <h7 className="createAcc"> dont have an account ?</h7>
   </div>
   
 
